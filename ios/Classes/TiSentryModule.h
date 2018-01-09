d98ce9a3e23c7a5df33b1f6463fdbc6f7641245f
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2018-01-09 14:03:04 +0100

    ios skeleton added

---
+++ ios/Classes/TiSentryModule.h
@@ -0,0 +1,14 @@
+/**
+ * sentry
+ *
+ * Created by Rainer
+ * Copyright (c) 2018 Your Company. All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@interface TiSentryModule : TiModule {
+
+}
+
+@end
